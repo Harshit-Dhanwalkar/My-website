eb9be3872e6609137a2ba92540139d7cb17fd03d
Harshit-Dhanwalkar <harshitpd1729@gmail.com>
2025-02-13 13:53:51 +0530

    Update nav bar

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 import Spheres2Background from "https://cdn.jsdelivr.net/npm/threejs-components@0.0.8/build/backgrounds/spheres2.cdn.min.js";
 
-// Initialize Sphere Packing Animation
 const bg = Spheres2Background(document.getElementById("webgl-canvas"), {
   count: 200,
   colors: [0xff0000, 0x000000, 0xffffff],
-  minSize: 0.3,
-  maxSize: 0.7,
+  minSize: 0.5,
+  maxSize: 1,
 });
 
-// Handle Color Change on Button Click
-document.getElementById("colors-btn").addEventListener("click", () => {
+const button1 = document.getElementById("colors-btn");
+
+document.body.addEventListener("click", (ev) => {
+  if (ev.target !== button1) bg.togglePause();
+});
+
+button1.addEventListener("click", () => {
   bg.spheres.setColors([
     0xffffff * Math.random(),
     0xffffff * Math.random(),
