74c2403677de1fb3f417b38981bd529836534b93
Harshit-Dhanwalkar <harshitpd1729@gmail.com>
2025-02-17 02:16:17 +0530

    Add custom cursors

--- script.js
+++ script.js
@@ -18,6 +18,13 @@ button1.addEventListener("click", () => {
   bg.spheres.light1.color.set(0xffffff * Math.random());
 });
 
+// custom cursor
+document.addEventListener("mousemove", function (e) {
+  const cursor = document.querySelector(".custom-cursor");
+  cursor.style.left = `${e.clientX}px`;
+  cursor.style.top = `${e.clientY}px`;
+});
+
 // scroll progress bar
 window.onscroll = function () {
   myFunction();
